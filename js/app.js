e6a4dc6af5651fb1e0f938650ef1df1acb146acb
halimov09 <sunnatullohhalimov>
2023-02-03 14:37:04 +0500

    the end

--- js/app.js
+++ js/app.js
@@ -3,17 +3,9 @@ const bar = document.querySelector("#bar"),
   menu = document.querySelector(".header__container--menu"),
   btnn = document.querySelector("#btnn");
 
-function show() {
-  menu.classList.add("show");
-  body.style.overflow = "hidden";
-}
-
-function delet() {
-  menu.classList.remove("show");
-  body.style.overflow = "scroll";
-}
-
 bar.addEventListener("click", () => {
-  show();
+  menu.classList.toggle("show");
 });
-btnn.addEventListener("click", delet);
+btnn.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
